6bb734cf3ac1b962941c2ee60747f7919e73c59b
hemengke <hemengke@snqu.com>
2019-05-27 18:10:05 +0800

    游戏介绍js还没完成

--- js/game-introduce.js
+++ js/game-introduce.js
@@ -1,12 +1,25 @@
 (function(){
-    $(window).scroll(function () {
-        var scrollHeight = $(window).scrollTop();
-        if(scrollHeight >= 70) {
-            $('.top').css('display','none');
-            $('.top_scroll').css('display','block');
-        } else {
-            $('.top').css('display','block');
-            $('.top_scroll').css('display','none');
-        }
-     });
+    var indexMode = {
+        init: function() {
+            this.totalEvents()
+        },
+        totalEvents: function () {
+            this.windowScrollFn()
+        },
+        windowScrollFn: function () {
+            $(window).scroll(function () {
+                var scrollHeight = $(window).scrollTop();
+                //console.log(scrollHeight);
+                if(scrollHeight >= 100) {
+                    $('.top').css('display','none');
+                    $('.top_scroll').css('display','block');
+                } else {
+                    $('.top').css('display','block');
+                    $('.top_scroll').css('display','none');
+                }
+             });
+        },
+        
+    }
+    indexMode.init();
 }())
