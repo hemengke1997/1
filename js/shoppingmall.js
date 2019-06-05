fb313a353e6623185ca4fc84a0eeb50116f8b8eb
hemengke <hemengke@snqu.com>
2019-06-05 18:02:34 +0800

    2019.6.5

--- js/shoppingmall.js
+++ js/shoppingmall.js
@@ -1,16 +1,4 @@
 (function () {
-    // var arr = [
-    //     {
-    //         type:1,
-    //         className:red
-
-    //     },
-    //     {
-    //         type:2,
-    //         className:blue
-
-    //     }
-    // ]
     var ShopMode = {
         init: function(){
             this.totalEvents()
@@ -24,7 +12,7 @@
             this.textFn();
             this.changeImgLabel();
             this.headActive();
-
+            this.cart();
         },
         windowScrollFn: function () {
             $(window).scroll(function () { 
@@ -108,29 +96,110 @@
             })
         },
         headActive: function () {
-            function close() {
-                $('.choose_role').css('display','none');
-            }
-            $('.arrow_back').click(close);
-
+            const headNumber = 5;
+            let imgSrc;
             //头像下标
-            var selected_item_index = 0;
+            let selected_item_index = 0;
+            let len = $('.role_pic').length;
+            let width = $('.role_head_li').width();
+
+
+           
+            
             // 鼠标进入的时候，当前头像active 其他头像取消active
             $('.role_head_li').mouseenter(function(){
                 $(this).siblings('li').find('img').removeClass('active');
                 $(this).find('img').addClass('active');
             })
-            // 1 鼠标离开的时候 恢复上一个选择的头像active状态  并把当前的头像取消active状态
-            $('.role_head_li').mouseleave(function(){
-                if($(this).index() === selected_item_index) {
-                    console.log(1);
+            $('.role_head_li').mouseleave(function () {
+                if($(this).index === selected_item_index) {
+                    return;
                 } else {
                     $(this).find('img').removeClass('active');
-                    $(this).eq(selected_item_index).find('img').addClass('active');
+                    $('.role_head_li').eq(selected_item_index).find('img').addClass('active');
+                }
+            })
+            // 1 鼠标离开的时候 恢复上一个选择的头像active状态  并把当前的头像取消active状态
+            $('.role_head_li').click(function(){
+                if($(this).index() !== selected_item_index) {
+                    //上一个选择的人物图隐藏
+                    $('.role_pic').eq(selected_item_index).css('display','none');
+                    //当前选择的人物图显示
+                    selected_item_index = $(this).index();
+                    addHeadStyle(selected_item_index);
+                }
+            })
+            // 删除头像的样式
+            function deleteHeadStyle(index){
+                $('.role_pic').eq(index).css('display','none');
+                $('.role_head_li').eq(index).find('img').removeClass('active');
+            }
+            // 增加头像样式
+            function addHeadStyle(index){
+                $('.role_head_li').eq(index).find('img').addClass('active');
+                imgSrc = $('.role_head_li').eq(index).find('img').attr('src');
+                $('.role_pic').eq(index).find('img').attr('src', imgSrc);
+                $('.role_pic').eq(index).css('display','flex');
+            }
+
+            $('.arrow_right').on('click',function(){
+                deleteHeadStyle(selected_item_index);
+                selected_item_index++;
+                console.log(selected_item_index);
+                if(selected_item_index <= len-1){
+                    addHeadStyle(selected_item_index);
+                    if(selected_item_index >= 3 && selected_item_index <= len-3) {
+                        $('.role_head_ul').css('transform','translateX('+ (-width - 10) * (selected_item_index - 2) +'px)')
+                    } else if(selected_item_index == len-2) {
+                        $('.role_head_ul').css('transform','translateX('+ (-width - 10) * (selected_item_index -3) +'px)')
+                    }
+                } else if (selected_item_index > len-1) {
+                    deleteHeadStyle(selected_item_index);
+                    selected_item_index = 0;
+                    addHeadStyle(selected_item_index);
+                    $('.role_head_ul').css('transform','translateX(0px)')
                 } 
             })
-            
+
+            $('.arrow_left').on('click',function () {
+                deleteHeadStyle(selected_item_index);
+                selected_item_index--;
+                console.log(selected_item_index);
+                if(selected_item_index >= 0){   
+                    addHeadStyle(selected_item_index);
+                    if(selected_item_index <= len-3 && selected_item_index > 2) {        
+                        $('.role_head_ul').css('transform','translateX('+ (-width - 10) * (selected_item_index - 2) +'px)')    
+                    } else if (selected_item_index <= 2) {
+                        $('.role_head_ul').css('transform','translateX(0px)');
+                    }
+                } else if (selected_item_index == -1) {
+                    selected_item_index = len - 1;
+                    addHeadStyle(selected_item_index);
+                    $('.role_head_ul').css('transform','translateX('+ (-width - 10) * (len - headNumber) +'px)')
+                }
+            })
+
+        },
+        cart: function () {
+            $('.close_cart').on('click',function(){
+                $('.choose_role').css('display','none');
+            });
+
+            $('.goods_item').on('mouseenter',function(){
+                $(this).addClass('active')
+            })
+            $('.goods_item').mouseleave(function(){
+                $(this).removeClass('active');
+            })
+
+
+            $('.goods_item').click(function () {
+                $('.role_right[type="cart"]').css('display','none');
+                let item_type = $(this)[0].type;
+                $('.role_right[type='+ item_type +']').css('display','block');
+            })    
         }
+        
     }
     ShopMode.init();
 }())
