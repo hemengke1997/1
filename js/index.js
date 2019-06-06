b758e22c11ba56d7090e8c6794036b0a660c4a35
hemengke <hemengke@snqu.com>
2019-06-06 18:03:17 +0800

    2019.6.6

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// $(function(){
+    
+
+//     //新闻tab
+//     $('.tab ul>li').mouseenter(function () { 
+//         $(this).addClass('active').siblings('li').removeClass('active');
+//         var index = $(this).index();
+//         console.log(index);
+//         $('.tab-ul').eq(index).addClass('selected').siblings('ul').removeClass('selected');
+//      })
+
+// });
