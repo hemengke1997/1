78987a682064a5cb79593e9ca14f269d829ef8ce
hemengke <hemengke@snqu.com>
2019-06-13 18:49:55 +0800

    “继续动态化

--- js/index.js
+++ js/index.js
@@ -1,53 +1,54 @@
-(function(){
+(function () {
     window.onload = function () {
         var indexMode = {
-            init: function() {
+            init: function () {
                 this.totalEvents();
             },
-            totalEvents: function() {
+            totalEvents: function () {
                 this.scrollFn();
                 this.mouseenterEvent();
                 this.animation();
                 this.carousel();
                 this.clickEvents();
                 this.videoEvents();
+                this.getNewsData();
             },
             // 滑轮滚动一定距离，头部变化
-            scrollFn: function() {
+            scrollFn: function () {
                 $(window).scroll(function () {
                     indexMode.animation();
-                 });
+                });
             },
             // 头部变化
             animation: function () {
                 var scrollHeight = $(window).scrollTop();
-                    if(scrollHeight >= 80) {
-                        $('.top').css('display','none');
-                        $('.top_scroll').css('display','block');
-                    } else {
-                        $('.top').css('display','block');
-                        $('.top_scroll').css('display','none');
-                    }
-                    if(scrollHeight >= 1100) {
-                        $('.third_person').animate({
-                            left:'-300px',
-                            bottom:'-135px'
-                        },1000)
-                    }
+                if (scrollHeight >= 80) {
+                    $('.top').css('display', 'none');
+                    $('.top_scroll').css('display', 'block');
+                } else {
+                    $('.top').css('display', 'block');
+                    $('.top_scroll').css('display', 'none');
+                }
+                if (scrollHeight >= 1100) {
+                    $('.third_person').animate({
+                        left: '-300px',
+                        bottom: '-135px'
+                    }, 1000)
+                }
             },
             mouseenterEvent: function () {
-                $('.news_item').mouseenter(function () {
-                    $('.news_box.active').find('.news_item').removeClass('active');
-                    $(this).addClass('active');
-                });
-                $('.news_tab_item').mouseenter(function (e) {
-                    let index = $(this).index();
-                    $('.news_tab_item').removeClass('active');
-                    $(this).addClass('active');
+                // $('.news_tab_item').click(function(){
 
-                    $('.news_box').removeClass('active').addClass('hide');
-                    $('.news_box').eq(index).removeClass('hide').addClass('active');
-                });
+                // })
+                // 鼠标进入新闻tab时 显示当前对应的新闻列表
+                // $('.news_tab_item').mouseenter(function (e) {
+                //     let index = $(this).index();
+                //     $('.news_tab_item').removeClass('active');
+                //     $(this).addClass('active');
+
+                //     $('.news_box').removeClass('active').addClass('hide');
+                //     $('.news_box').eq(index).removeClass('hide').addClass('active');
+                // });
                 $('.video_tab_li').mouseenter(function () {
                     $('.video_tab_li').removeClass('active');
                     $(this).addClass('active');
@@ -62,44 +63,44 @@
             },
             clickEvents: function () {
                 let video = document.getElementsByClassName('video');
-                $('.video_button').click(function(){
+                $('.video_button').click(function () {
                     let v_attr = $(this).parent().siblings('video').attr('src');
-                    $('.big_video_box').find('video').attr('src',v_attr);
+                    $('.big_video_box').find('video').attr('src', v_attr);
                     $('.big_video_box').show();
                 });
-                $('.close_video').click(()=>{
-                    $('.big_video_box').find('video').attr('src','');
+                $('.close_video').click(() => {
+                    $('.big_video_box').find('video').attr('src', '');
                     $('.big_video_box').hide();
                 })
             },
             videoEvents: function () {
                 let v = document.getElementsByClassName('video');
-                
+
             },
             // 轮播图
-            carousel: function(){
+            carousel: function () {
                 let flag = true;
                 let index = 1;
                 $('.carousel_item').last().clone().insertBefore($('.carousel_item').first());
                 $('.carousel_item').eq(1).clone().appendTo('.carousel_ul');
                 let len = $('.carousel_item').length;
                 let p_width = $('.carousel_item img').width();
-                $('.carousel_ul').css('left',''+ -p_width +'');
-                let timerID = setInterval(selectPic,3000);
-                $('.carousel_box').mouseenter(()=>{
+                $('.carousel_ul').css('left', '' + -p_width + '');
+                let timerID = setInterval(selectPic, 3000);
+                $('.carousel_box').mouseenter(() => {
                     clearInterval(timerID);
-                }).mouseleave(()=>{
-                    timerID = setInterval(selectPic,3000);
-                })    
+                }).mouseleave(() => {
+                    timerID = setInterval(selectPic, 3000);
+                })
                 // 自动切换图片
-                function selectPic(){
+                function selectPic() {
                     // 初始   
 
-                    if(index < (len - 2) && index > 0) {
+                    if (index < (len - 2) && index > 0) {
                         flag = false;
                         $('.carousel_ul').animate({
-                            left:''+ (-p_width) * (index + 1) + 'px'
-                        },800,'linear',function(){
+                            left: '' + (-p_width) * (index + 1) + 'px'
+                        }, 800, 'linear', function () {
                             flag = true;
                         })
                         index++;
@@ -107,46 +108,144 @@
                     } else if (index === (len - 2)) {
                         flag = false;
                         $('.carousel_ul').animate({
-                            left:''+ (-p_width) * (index + 1) + 'px'
-                        },800,'linear',function () {
-                            $('.carousel_ul').css('left',-p_width);
+                            left: '' + (-p_width) * (index + 1) + 'px'
+                        }, 800, 'linear', function () {
+                            $('.carousel_ul').css('left', -p_width);
                             flag = true;
-                        });          
+                        });
                         index = 1;
-                    }                
+                    }
                 }
-                $('.arrow_right').click(function(){
-                    if(flag) {
+                $('.arrow_right').click(function () {
+                    if (flag) {
                         clearInterval(timerID);
                         selectPic();
-                        timerID =  setInterval(selectPic,3000);
+                        timerID = setInterval(selectPic, 3000);
                     }
                 });
-                $('.arrow_left').click(function(){
-                    if(flag) {
+                $('.arrow_left').click(function () {
+                    if (flag) {
                         flag = !flag;
                         clearInterval(timerID);
-                        if(index === 1){
+                        if (index === 1) {
                             $('.carousel_ul').animate({
-                                left:'0px'
-                            },800,'linear',function(){
-                                $('.carousel_ul').css('left', -(len-2) * p_width);
+                                left: '0px'
+                            }, 800, 'linear', function () {
+                                $('.carousel_ul').css('left', -(len - 2) * p_width);
                                 flag = !flag;
                             })
-                            index = len-2;
+                            index = len - 2;
                         } else {
                             $('.carousel_ul').animate({
-                                left: ''+ (-p_width) * (index - 1) +'px'
-                            },800,'linear',function(){
+                                left: '' + (-p_width) * (index - 1) + 'px'
+                            }, 800, 'linear', function () {
                                 flag = !flag;
                             })
                             index--;
                         }
-                        timerID = setInterval(selectPic,3000);
+                        timerID = setInterval(selectPic, 3000);
                     }
                 })
-                
+            },
+            // 页面加载之后，读取新闻列表（包括类型，title，date）
+            // 最新的
+            getNewsData: function () {
+                function getDate(time) {
+                    let date = new Date(time * 1000);
+                    let year = date.getFullYear();
+                    let month = ((date.getMonth() + 1) >= 10) ? date.getMonth() + 1 : '0' + (date.getMonth() + 1).toString();
+                    let day = (date.getDay() >= 10) ? date.getDay() : '0' + (date.getDay().toString());
+                    return month + '-' + day;
+                }
+                let li_len = 0;
+                $.ajax({
+                    type: "get",
+                    url: "http://api.paopao.vip/news/item",
+                    dataType: "json",
+                    success: function (response) {
+                        if (response.code === 0) {
+                            var all_li = '';
+                            var data = response.data.records;
+                            if (data.length > 5) {
+                                li_len = 5;
+                                for (let i = 0; i < li_len; i++) {
+                                    all_li += '<li class="news_item" id="'+ data[i]._id +'">' +
+                                        '<span class="news_icon">' + data[i].type + '</span>' +
+                                        '<div class="news">' +
+                                        '<span class="news_content">' + data[i].title + '</span>' +
+                                        '<span class="news_date">' + getDate(data[i].itime) + '</span>' +
+                                        '</div>' +
+                                        '</li>';
+                                }
+                            } else if (data.length > 0 && data.length <= 5) {
+                                for (let i = 0; i < data.length; i++) {
+                                    all_li += '<li class="news_item" id="'+ data[i]._id +'">' +
+                                        '<span class="news_icon">' + data[i].type + '</span>' +
+                                        '<div class="news">' +
+                                        '<span class="news_content">' + data[i].title + '</span>' +
+                                        '<span class="news_date">' + getDate(data[i].itime) + '</span>' +
+                                        '</div>' +
+                                        '</li>'
+                                }
+                            }
+                            $('.news_ul').html(all_li);
+                            $('.news_item').first().addClass('active');
+                            $('.news_item').mouseenter(function () {
+                                $('.news_item').removeClass('active');
+                                $(this).addClass('active');
+                            });
+
+                        }
+                    }
+                });
+
+                // 获取新闻或者活动列表
+                $('.news_tab_item').click(function () {
+                    if (!($(this).hasClass('active'))) {
+                        var newsType = $(this).find('.news_ch').text();
+                        $('.news_tab_item').removeClass('active');
+                        $(this).addClass('active');
+                        if (newsType == '最新') {
+                            indexMode.getNewsData();
+                        } else {
+                            $.ajax({
+                                type: "get",
+                                url: "http://api.paopao.vip/news/item",
+                                dataType: "json",
+                                success: function (response) {
+                                    if (response.code === 0 && response.data.count > 0) {
+                                        var theCount = 0;
+                                        var all_li = '';
+                                        var data = response.data.records;
+                                        for (let i = 0; i < data.length && theCount < 5; i++) {
+                                            if (data[i].type == newsType) {
+                                                theCount++;
+                                                all_li += '<li class="news_item" id="'+ data[i]._id +'">' +
+                                                    '<span class="news_icon">' + data[i].type + '</span>' +
+                                                    '<div class="news">' +
+                                                    '<span class="news_content">' + data[i].title + '</span>' +
+                                                    '<span class="news_date">' + getDate(data[i].itime) + '</span>' +
+                                                    '</div>' +
+                                                    '</li>';
+                                            }
+                                        }
+
+                                        $('.news_ul').html(all_li);
+                                        $('.news_item').first().addClass('active');
+                                        $('.news_item').mouseenter(function () {
+                                            $('.news_item').removeClass('active');
+                                            $(this).addClass('active');
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    }
+                })
+
+
             }
+
         }
         indexMode.init();
     }
--- js/news-center.js
+++ js/news-center.js
@@ -1,75 +1,167 @@
-(function(){
-    window.onload=function() {
-        var indexMode = {
-            init: function() {
+(function () {
+    window.onload = function () {
+        var newsCenterMode = {
+            init: function () {
                 this.totalEvents();
             },
-            totalEvents: function() {
-                this.newsLiHoverFn();
+            totalEvents: function () {
                 this.scrollFn();
                 this.returnFn();
-                this.showNewsFn();
-                this.tabToggleFn();
+                this.firstTimeGetNews();
+                this.getNewsList();
+
             },
-            // li标签的hover效果
-            newsLiHoverFn: function () {
-                var news_li = $('.news_li');
-                $(news_li).mouseenter(function () {
-                    $(news_li).removeClass('active');
-                    $(this).addClass('active');
-                    $(this).find('.container').css('left','-50px');
-                    $(this).find('.news_date').css('display','none');
-                    $(this).find('.news_arrow').css('display','block');
-                }).mouseleave(function () { 
-                    $(this).find('.container').css('left','0px');
-                    $(this).find('.news_date').css('display','block');
-                    $(this).find('.news_arrow').css('display','none');
-                });
-            },
+
             // 滑轮滚动一定距离，头部变化
-            scrollFn: function() {
+            scrollFn: function () {
                 $(window).scroll(function () {
                     var scrollHeight = $(window).scrollTop();
                     //console.log(scrollHeight);
-                    if(scrollHeight >= 80) {
-                        $('.top').css('display','none');
-                        $('.top_scroll').css('display','block');
+                    if (scrollHeight >= 80) {
+                        $('.top').css('display', 'none');
+                        $('.top_scroll').css('display', 'block');
                     } else {
-                        $('.top').css('display','block');
-                        $('.top_scroll').css('display','none');
+                        $('.top').css('display', 'block');
+                        $('.top_scroll').css('display', 'none');
                     }
-                 });
+                });
             },
             // 点击返回，回到新闻列表
-            returnFn: function () { 
-                $('.return_arrow').click(function () { 
-                    $('.news_body2').css('display','none');
-                    $('.news_body1').css('display','block');
+            returnFn: function () {
+                $('.return_arrow').click(function () {
+                    $('.news_body2').css('display', 'none');
+                    $('.news_body1').css('display', 'block');
                     return false;
-                });  
-            },
-            // 点击li标签，显示对应新闻
-            showNewsFn: function () {
-                $(".news_detail li").click(function(){
-                    console.log($(this).index());
-                    var index = $(this).index();
-                    $('.news_body1').css('display','none');
-                    $('.news_body2').eq(index).css('display','block');
                 });
             },
-            // tab效果
-            tabToggleFn: function () {
-                $('#tab_ul li').mouseenter(function () { 
-                    var index = $(this).index();
-                    $(this).siblings().find('span').removeClass('active');
-                    $(this).find('span').addClass('active');
-                    $('.main_2').addClass('hide').removeClass('active');
-                    $('.main_2').eq(index).removeClass('hide').addClass('active');
-                    $('.main_2').eq(index).find('.news_li').eq(0).addClass('active');
-                    $('.main_2').eq(index).find('.news_li').eq(0).siblings().removeClass('active');
+            // 第一次获取的新闻
+            firstTimeGetNews: function () {
+                $('.news_li').click(function () {
+                    console.log(1);
+                })
+                $.ajax({
+                    type: "get",
+                    url: "http://api.paopao.vip/news/item",
+                    dataType: "json",
+                    success: function (response) {
+                        var data = response.data.records;
+                        if (response.data.count > 0) {
+                            var all_li = '';
+                            for (let i = 0; i < data.length; i++) {
+                                all_li += '<li class="news_li" id="' + data[i]._id + '">' +
+                                    '<div class="container">' +
+                                    '<div class="news_text">' +
+                                    '<span class="news_logo">' + data[i].type + '</span>' +
+                                    '<div class="news_text_head">' + data[i].name + '</div>' +
+                                    '<div class="news_text_body">' + data[i].description + '</div>' +
+                                    '</div>' +
+                                    '<div class="news_date">' + newsCenterMode.getDate(data[i].itime) + '</div>' +
+                                    '<div class="news_arrow"></div>' +
+                                    '</div>' +
+                                    '</li>';
+
+                            }
+
+                            $('.news_detail').html(all_li);
+                            $('.news_li').first().addClass('active');
+                            $('.news_li').mouseenter(function () {
+                                $('.news_li').removeClass('active');
+                                $(this).addClass('active');
+                                $(this).find('.container').css('left', '-50px');
+                                $(this).find('.news_date').css('display', 'none');
+                                $(this).find('.news_arrow').css('display', 'block');
+                            }).mouseleave(function () {
+                                $(this).find('.container').css('left', '0px');
+                                $(this).find('.news_date').css('display', 'block');
+                                $(this).find('.news_arrow').css('display', 'none');
+                            });
+                            $('.news_li').click(function () {
+                                newsCenterMode.goToNewsDetails($(this));
+                            })
+                        }
+                    }
                 });
+            },
+            getNewsList: function () {
+                $('.news_tab_item').click(function () {
+                    // 切换tab样式
+                    if (!$(this).hasClass('active')) {
+                        $('.news_tab_item').removeClass('active');
+                        $('.yl_bg').removeClass('active');
+                        $(this).addClass('active');
+                        $(this).find('.yl_bg').addClass('active');
+                        var newsType = $(this).find('.zh_text').text();
+                        // ajax请求
+                        if (newsType == '最新') {
+                            newsCenterMode.firstTimeGetNews();
+                        } else {
+                            $.ajax({
+                                type: "get",
+                                url: "http://api.paopao.vip/news/item",
+                                dataType: "json",
+                                success: function (response) {
+                                    if (response.code === 0 && response.data.count > 0) {
+                                        var data = response.data.records;
+                                        var all_li = '';
+                                        for (let i = 0, len = data.length; i < len; i++) {
+                                            if (response.data.records[i].type == newsType) {
+                                                all_li += '<li class="news_li" id="' + data[i]._id + '">' +
+                                                    '<div class="container">' +
+                                                    '<div class="news_text">' +
+                                                    '<span class="news_logo">' + data[i].type + '</span>' +
+                                                    '<div class="news_text_head">' + data[i].name + '</div>' +
+                                                    '<div class="news_text_body">' + data[i].description + '</div>' +
+                                                    '</div>' +
+                                                    '<div class="news_date">' + newsCenterMode.getDate(data[i].itime) + '</div>' +
+                                                    '<div class="news_arrow"></div>' +
+                                                    '</div>' +
+                                                    '</li>';
+                                            }
+                                        }
+                                        $('.news_detail').html(all_li);
+                                        $('.news_li').first().addClass('active');
+                                        $('.news_li').mouseenter(function () {
+                                            $('.news_li').removeClass('active');
+                                            $(this).addClass('active');
+                                            $(this).find('.container').css('left', '-50px');
+                                            $(this).find('.news_date').css('display', 'none');
+                                            $(this).find('.news_arrow').css('display', 'block');
+                                        }).mouseleave(function () {
+                                            $(this).find('.container').css('left', '0px');
+                                            $(this).find('.news_date').css('display', 'block');
+                                            $(this).find('.news_arrow').css('display', 'none');
+                                        })
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            },
+            getDate: function (time) {
+                let date = new Date(time * 1000);
+                let year = date.getFullYear();
+                let month = ((date.getMonth() + 1) >= 10) ? date.getMonth() + 1 : '0' + (date.getMonth() + 1).toString();
+                let day = (date.getDay() >= 10) ? date.getDay() : '0' + (date.getDay().toString());
+                return month + '-' + day;
+            },
+            goToNewsDetails: function (_this) {
+                var _id = _this.attr('id');
+                console.log(_id);
+                $.ajax({
+                    type: "get",
+                    url: "http://api.paopao.vip/news/item/one",
+                    data:{
+                        id: _id
+                    },
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+                        console.log(_id);
+                    }
+                })
             }
         }
-        indexMode.init();
+        newsCenterMode.init();
     }
 }())
